dec1cdbf24f3756c0fffdaa8ed50e29691365ded
Atul Kumar <atulkumar65@gmail.com>
2022-01-08 17:40:43 +0530

    updated v1.03

--- script.js
+++ script.js
@@ -12,9 +12,13 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var si = principal*rate*years/100;
-    var year = new Date().getFullYear()+parseInt(years);;
-
-    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br> at an interest of <mark>${rate}%</mark>.<br> You will receive an amount of <mark>${si.toFixed()}</mark>,<br> in the year <mark>${year}</mark>`;
+    if(principal<=0)
+    {
+        alert("Please enter a positive number as Principal Amount")
+    } else {
+        var si = principal*rate*years/100;
+        var year = new Date().getFullYear()+parseInt(years);;
+        document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br> at an interest of <mark>${rate}%</mark>.<br> You will receive an amount of <mark>${si.toFixed()}</mark>,<br> in the year <mark>${year}</mark>`;
+    }
 }
         
