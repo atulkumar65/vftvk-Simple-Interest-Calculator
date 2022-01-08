44a5c2e51f9bb08bdd2deecb5ac9e63ab693c9b7
Atul Kumar <atulkumar65@gmail.com>
2022-01-08 16:51:14 +0530

    completed project

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+window.onload = function() {
+    document.getElementById("myrate").innerHTML = `${document.getElementById("rate").value}%`;
+}
+
+function sliderChange(){
+    var slider = document.getElementById("rate");
+    document.getElementById("myrate").innerHTML = `${slider.value} %`;
+}
+
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    si = p*(1+(r/100)*t) - p;
+    year = 202 + t;
+
+    document.getElementById("result").innerHTML = `If you deposit <mark>${p}</mark>,<br> at an interest of <mark>${r}%</mark>.<br> You will receive an amount of <mark>${si.toFixed()}</mark>,<br> in the year <mark>${year}</mark>`;
 }
         
